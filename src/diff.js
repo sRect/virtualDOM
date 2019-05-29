fbe22f1d922c6345c1406d32d66071ead13374c8
Fang Chaoqun <fcqun@sina.cn>
2019-05-29 18:06:46 +0800

    :construction: feat: diff进行中

--- src/diff.js
+++ src/diff.js
@@ -1,5 +1,9 @@
 import * as types from './types.js';
 
+function isString(node) {
+  return Object.prototype.toString.call(node) === '[object String]';
+}
+
 function diff(oldTree, newTree) {
   let patches = {};
   let index = 0;
@@ -8,6 +12,7 @@ function diff(oldTree, newTree) {
   return patches;
 }
 
+// 比较属性差异
 function diffAttr(oldAttrs, newAttrs) {
   let patch = {};
 
@@ -27,24 +32,39 @@ function diffAttr(oldAttrs, newAttrs) {
   return patch;
 }
 
+// 比较子节点差异
+function diffChildren(oldChild, newChild, patches) {
+  
+}
+
 // 遍历树 找出差异 放入补丁包中
 function walk(oldNode, newNode, index, patches) {
   let currentPatch = [];
 
-  if(oldNode.type === newNode.type) {
+  if(!newNode) { // 节点删除
+    currentPatch.push({type: types.REMOVE, index});
+  } else if (isString(oldNode) && isString(newNode)) { // 判断文本变化
+    currentPatch.push({ type: types.TEXT,text: newNode });
+  }else if(oldNode.type === newNode.type) {
     // 比较属性差异
     let attrs = diffAttr(oldNode.props, newNode.props);
     if(Object.keys(attrs).length) { // 判断是否为一个空对象
       currentPatch.push({type: types.ATTRS, attrs})
     }
 
-    if (currentPatch.length) {
-      // 将当前元素补丁放入到大补丁中
-      patches[index] = currentPatch;
-    }
-
-    console.log(patches)
+    // 如果有子节点应该遍历子节点
+    diffChildren(oldNode.children, newNode.children, patches);
+  } else {
+    // 节点被替换
+    currentPatch.push({ type: REPLACE, newNode });
   }
+
+  if (currentPatch.length) {
+    // 将当前元素补丁放入到大补丁中
+    patches[index] = currentPatch;
+  }
+
+  console.log(patches)
 } 
 
 export default diff;
--- src/types.js
+++ src/types.js
@@ -1,1 +1,4 @@
-export const ATTRS = 'ATTRS';
+export const ATTRS = 'ATTRS'; // 属性变化
+export const TEXT = 'TEXT'; // 文本变化
+export const REMOVE = 'REMOVE'; // 节点删除
+export const REPLACE = 'REPLACE'; // 节点覆盖
